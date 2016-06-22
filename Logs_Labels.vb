c5be7ea95f2f2171deb98bd9c42c44a36079e7e9
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2016-06-22 13:39:20 +0100

    Initial commit

---
+++ Logs_Labels.vb
@@ -0,0 +1,16 @@
+Sub cy10_zsd19()
+
+'1. Find report's lower boundry
+'2. Find report's upper boundry and location of the data columns (Material, Quantity)
+'3. Collect data into an array
+'4. Print array on the "ALL" tab for the labels to read from
+
+End Sub
+
+Sub zd134()
+
+'1. Collect all customers from all zd134 tabs (CY10, GT10, BP10) into an array
+'2. Sort array by customer account number
+'3. Print unique entries from the table into the Master Sheet
+
+End Sub
