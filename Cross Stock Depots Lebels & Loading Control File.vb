57ee924088f15079570815a15d22507e7c48e966
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2016-08-05 16:56:01 +0100

    Inserting page breaks and clearing data

--- Cross Stock Depots Lebels & Loading Control File.vb
+++ Cross Stock Depots Lebels & Loading Control File.vb
@@ -19,17 +19,19 @@ With Sheet1
     arr_counter = 1
     'Iterate through data, report starts from row 8
     For counter = report_start To lastrow
-        ' Route number
-        customers(arr_counter, 1) = .Cells(counter, 1).Value
-        ' Drop number
-        customers(arr_counter, 2) = .Cells(counter, 3).Value
-        ' Customer name
-        customers(arr_counter, 3) = .Cells(counter, 10).Value
-        ' Frozen volume
-        customers(arr_counter, 4) = .Cells(counter, 17).Value
-        ' Chilled and ambient volume
-        customers(arr_counter, 5) = .Cells(counter, 20).Value
-        arr_counter = arr_counter + 1
+        If Len(Trim(.Cells(counter, 1).Value)) = 6 Then
+            ' Route number
+            customers(arr_counter, 1) = .Cells(counter, 1).Value
+            ' Drop number
+            customers(arr_counter, 2) = .Cells(counter, 3).Value
+            ' Customer name
+            customers(arr_counter, 3) = .Cells(counter, 10).Value
+            ' Frozen volume
+            customers(arr_counter, 4) = .Cells(counter, 17).Value
+            ' Chilled and ambient volume
+            customers(arr_counter, 5) = .Cells(counter, 20).Value
+            arr_counter = arr_counter + 1
+        End If
     Next counter
 End With
 
@@ -38,8 +40,8 @@ With Sheet2
     ' Report starts from row 3
     counter = 3
     ' Clear report
-    .Range(.Cells(3, 1), .Cells(501, 2)).ClearContents
-    .Range(.Cells(3, 4), .Cells(501, 6)).ClearContents
+    .Range(.Cells(3, 1), .Cells(10000, 2)).ClearContents
+    .Range(.Cells(3, 4), .Cells(10000, 6)).ClearContents
     ' Write data from array to report
     For arr_counter = 1 To UBound(customers, 1)
         If customers(arr_counter, 1) = Empty Then Exit For
@@ -57,6 +59,8 @@ With Sheet2
         counter = counter + 1
     Next arr_counter
 End With
+
+Call insert_page_breaks
 
 ' Garbage clearout
 Erase customers
@@ -642,3 +646,37 @@ With Sheet8
 End With
 
 End Sub
+
+Sub insert_page_breaks()
+
+Dim lastrow As Long, counter As Long
+
+With Sheet2
+    lastrow = .Cells(Rows.Count, 1).End(xlUp).Row
+    ' Report starts from row 3, slip first row
+    .ResetAllPageBreaks
+    For counter = 4 To lastrow
+        If Left(.Cells(counter, 1).Value, 2) = Left(.Cells(counter - 1, 1).Value, 2) Then
+            If Left(.Cells(counter, 1).Value, 2) = "RG" Then
+                If .Cells(counter, 1).Value <> .Cells(counter - 1, 1).Value Then
+                    .Rows(counter).PageBreak = xlPageBreakManual
+                End If
+            End If
+        Else
+            .Rows(counter).PageBreak = xlPageBreakManual
+        End If
+    Next counter
+    .PageSetup.PrintArea = "$a$1:$j" & lastrow
+End With
+
+End Sub
+
+Sub clear_sheet2()
+
+With Sheet2
+    .Range(.Cells(3, 1), .Cells(10000, 2)).ClearContents
+    .Range(.Cells(3, 4), .Cells(10000, 6)).ClearContents
+    .ResetAllPageBreaks
+End With
+End Sub
+
