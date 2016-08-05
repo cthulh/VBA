c05523d72a3f76361dda736d3879e9ed4dae46f3
Mariusz Borowiak <tryglavson@hotmail.co.uk>
2016-08-05 08:01:07 +0100

    Initial commit

---
+++ Cross Stock Depots Lebels & Loading Control File.vb
@@ -0,0 +1,16 @@
+Sub main_procedure()
+
+Dim customers() As Variant
+Dim counter As Long, lastrow As Long
+
+'1. Scoop all customer data from a ZL18 download (data is sorted by route)
+
+With Sheet1
+    
+End With
+
+'2. Write data into tab "ALL"
+
+
+
+End Sub
